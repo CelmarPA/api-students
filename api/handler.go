97fec7c224c4728b7754a2899b6f39d5fb2c7264
Celmar Pereira <celmarpereiradeandrade@gmail.com>
2025-11-17 17:39:48 -0300

    Filtered Students route

--- api/handler.go
+++ api/handler.go
@@ -19,7 +19,21 @@ func (api *API) getStudents(c echo.Context) error {
 		return c.String(http.StatusNotFound, "Failed to get students")
 	}
 
-  	return c.JSON(http.StatusOK, students)
+	active := c.QueryParam("active")
+
+	if active != "" {
+		act, err := strconv.ParseBool(active)
+		if err != nil {
+			log.Error().Err(err).Msgf("[api] erro to parse boolean")
+			return c.String(http.StatusInternalServerError, "Failed to parse boolean")
+		}
+		students, err = api.DB.GetFilteredStudents(act)
+
+	}
+
+	listOfStudents := map[string][]schemas.StudentResponse{"students:": schemas.NewResponse(students)}
+
+  	return c.JSON(http.StatusOK, listOfStudents)
 }
 
 func (api *API) createStudent(c echo.Context) error {
--- schemas/shcemas.go
+++ schemas/shcemas.go
@@ -2,6 +2,7 @@ package schemas
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
 
 type Student struct {
@@ -11,4 +12,36 @@ type Student struct {
 	Email string `json:"email"`
 	Age int      `json:"age"`
 	Active bool  `json:"active"`
+}
+
+type StudentResponse struct {
+	ID int `json:"id"`
+	CreatedAt time.Time `json:"createAt"`
+	UpdatedAt time.Time `json:"updateAt"`
+	// DeletedAt time.Time `json:"deleteAt"`
+	Name string  `json:"name"`
+	CPF int      `json:"cpf"`
+	Email string `json:"email"`
+	Age int      `json:"age"`
+	Active bool  `json:"active"`
+}
+
+func NewResponse(students []Student) []StudentResponse {
+	studentsResponse := []StudentResponse{}
+
+	for _, student := range students {
+		studentResponse := StudentResponse{
+			ID: int(student.ID),
+			CreatedAt: student.CreatedAt,
+			UpdatedAt: student.UpdatedAt,
+			Name: student.Name,
+			CPF: student.CPF,
+			Email: student.Email,
+			Age: student.Age,
+			Active: student.Active,
+		}
+		studentsResponse = append(studentsResponse, studentResponse)
+	}
+
+	return studentsResponse
 }
